bdb4d68c1d624bea1a6c24904b79a84e1803acf8
Keyur Patel <keyur.patel@shcc.com.au>
2024-09-17 13:03:54 +0800

    routing

---
+++ app/(routegroup)/layout1/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return ( 
+        <div>
+            This is a home page from Layout 1
+        </div>
+     );
+}
+ 
+export default HomePage;
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return ( 
+        <div>
+            <p className="text-3xl font-medium text-sky-700">This is a test page</p>
+        </div>
+     );
+}
+ 
+export default TestPage;
